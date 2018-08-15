d3dea67ade16a7b2ba01dd168e0f5c70b4c6f6e4
aikobin <582748391@qq.com>
2018-08-15 16:02:13 +0800

    ceshi

--- src/main/java/com/bin/gateway/filter/LogFilter.java
+++ src/main/java/com/bin/gateway/filter/LogFilter.java
@@ -15,7 +15,7 @@ public class LogFilter extends ZuulFilter {
 	public Object run() {
 		RequestContext ctx = RequestContext.getCurrentContext();
 		HttpServletRequest request = ctx.getRequest();
-		log.info(String.format("请求信息 ： send %s request to %s", request.getMethod(), request.getRequestURL().toString()));
+		log.info(String.format("请求信息 ：ip %s send %s request to %s",getIPAddress(request), request.getMethod(), request.getRequestURL().toString()));
 		return null;
 	}
 
@@ -32,5 +32,43 @@ public class LogFilter extends ZuulFilter {
 	@Override
 	public String filterType() {
 		return "pre";
+	}
+	
+	public  String getIPAddress(HttpServletRequest request) {
+	    String ip = null;
+
+	    //X-Forwarded-For：Squid 服务代理
+	    String ipAddresses = request.getHeader("X-Forwarded-For");
+
+	    if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+	        //Proxy-Client-IP：apache 服务代理
+	        ipAddresses = request.getHeader("Proxy-Client-IP");
+	    }
+
+	    if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+	        //WL-Proxy-Client-IP：weblogic 服务代理
+	        ipAddresses = request.getHeader("WL-Proxy-Client-IP");
+	    }
+
+	    if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+	        //HTTP_CLIENT_IP：有些代理服务器
+	        ipAddresses = request.getHeader("HTTP_CLIENT_IP");
+	    }
+
+	    if (ipAddresses == null || ipAddresses.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+	        //X-Real-IP：nginx服务代理
+	        ipAddresses = request.getHeader("X-Real-IP");
+	    }
+
+	    //有些网络通过多层代理，那么获取到的ip就会有多个，一般都是通过逗号（,）分割开来，并且第一个ip为客户端的真实IP
+	    if (ipAddresses != null && ipAddresses.length() != 0) {
+	        ip = ipAddresses.split(",")[0];
+	    }
+
+	    //还是不能获取到，最后再通过request.getRemoteAddr();获取
+	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ipAddresses)) {
+	        ip = request.getRemoteAddr();
+	    }
+	    return ip;
 	}
 }
